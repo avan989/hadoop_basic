ce29936685b5b3a658a36a81a490f358b0a83d90
anh <avan989@yahoo.com>
2020-09-07 22:07:06 -0400

    update for hive

--- startup.sh
+++ startup.sh
@@ -3,6 +3,14 @@
 /etc/bootstrap.sh
 jps
 
+$HADOOP_HOME/bin/hadoop dfs -mkdir -p /tmp
+$HADOOP_HOME/bin/hadoop dfs -mkdir -p /user/hive/warehouse
+$HADOOP_HOME/bin/hadoop dfs -chmod g+w /user/hive/warehouse
+$HADOOP_HOME/bin/hadoop dfs -chmod g+w /tmp
+
+schematool -dbType derby -initSchemaTo 2.0.0 -verbose
+
+echo "Finish Setting Up"
 while true
 do
   :
