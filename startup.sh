dd99645a276e58d06ae1ea99adc7f1aa40f0c385
anh <avan989@yahoo.com>
2020-09-13 17:10:59 -0400

    add spark and scala

--- startup.sh
+++ startup.sh
@@ -7,8 +7,9 @@ $HADOOP_HOME/bin/hadoop dfs -mkdir -p /tmp
 $HADOOP_HOME/bin/hadoop dfs -mkdir -p /user/hive/warehouse
 $HADOOP_HOME/bin/hadoop dfs -chmod g+w /user/hive/warehouse
 $HADOOP_HOME/bin/hadoop dfs -chmod g+w /tmp
-
 schematool -dbType derby -initSchemaTo 2.0.0 -verbose
+
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master --ip `hostname` --port 7077 --webui-port 8080
 
 echo "Finish Setting Up"
 while true
