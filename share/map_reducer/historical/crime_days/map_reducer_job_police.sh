b69edd684187f038a4d09df6df8d8b3428ea0b8d
avan989 <avan989@yahoo.com>
2020-11-01 19:53:17 -0500

    update ignore

--- share/map_reducer/historical/crime_days/map_reducer_job_police.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hadoop jar /usr/local/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-*streaming*.jar \
--file /usr/local/share/map_reducer/police_mapper.py    -mapper /usr/local/share/map_reducer/police_mapper.py \
--file /usr/local/share/map_reducer/police_reducer.py   -reducer /usr/local/share/map_reducer/police_reducer.py \
--input /tmp/police_reports.csv -output /tmp/police_reports_output
